c8f57398462f4cb0feb1d1da192a457b9b3a43c5
Robin Ebert <ebertrobin2002@gmail.com>
2021-09-19 20:34:41 +0200

    Pacman only allows one EXEC line

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@
 
 sudo mkdir -p /etc/pacman.d/hooks
 sudo ln 92-sign_unified_image.hook /etc/pacman.d/hooks/92-sign_unified_image.hook
+sudo ln 92-sign_unified_image_fallback.hook /etc/pacman.d/hooks/92-sign_unified_image_fallback.hook
